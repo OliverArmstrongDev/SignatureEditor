0ebe77e51c1ac4cb100495c78b316fa91660b4a8
Oliver Armstrong <99773634+OliverArmstrongDev@users.noreply.github.com>
2022-06-19 15:23:33 +1200

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SignatureEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAction = new System.Windows.Forms.Button();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtMob = new System.Windows.Forms.TextBox();
+            this.txtFax = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblMob = new System.Windows.Forms.Label();
+            this.lblFax = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblJobTitle = new System.Windows.Forms.Label();
+            this.txtJobTitle = new System.Windows.Forms.TextBox();
+            this.lblExtraTxt = new System.Windows.Forms.Label();
+            this.txtExtra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(196, 369);
+            this.btnAction.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(75, 23);
+            this.btnAction.TabIndex = 9;
+            this.btnAction.Text = "Go";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(91, 25);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(180, 20);
+            this.txtFname.TabIndex = 0;
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(91, 68);
+            this.txtLname.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(180, 20);
+            this.txtLname.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(91, 154);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(105, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtMob
+            // 
+            this.txtMob.Location = new System.Drawing.Point(91, 197);
+            this.txtMob.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtMob.Name = "txtMob";
+            this.txtMob.Size = new System.Drawing.Size(105, 20);
+            this.txtMob.TabIndex = 4;
+            // 
+            // txtFax
+            // 
+            this.txtFax.Location = new System.Drawing.Point(91, 240);
+            this.txtFax.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtFax.Name = "txtFax";
+            this.txtFax.Size = new System.Drawing.Size(105, 20);
+            this.txtFax.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(91, 283);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(34, 28);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(52, 13);
+            this.lblFName.TabIndex = 8;
+            this.lblFName.Text = "Firstname";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(33, 71);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(53, 13);
+            this.lblLName.TabIndex = 9;
+            this.lblLName.Text = "Lastname";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(34, 157);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(59, 13);
+            this.lblPhone.TabIndex = 10;
+            this.lblPhone.Text = "Phone (07)";
+            // 
+            // lblMob
+            // 
+            this.lblMob.AutoSize = true;
+            this.lblMob.Location = new System.Drawing.Point(34, 200);
+            this.lblMob.Name = "lblMob";
+            this.lblMob.Size = new System.Drawing.Size(38, 13);
+            this.lblMob.TabIndex = 11;
+            this.lblMob.Text = "Mobile";
+            // 
+            // lblFax
+            // 
+            this.lblFax.AutoSize = true;
+            this.lblFax.Location = new System.Drawing.Point(34, 243);
+            this.lblFax.Name = "lblFax";
+            this.lblFax.Size = new System.Drawing.Size(45, 13);
+            this.lblFax.TabIndex = 12;
+            this.lblFax.Text = "Fax (07)";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(34, 286);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 13;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblJobTitle
+            // 
+            this.lblJobTitle.AutoSize = true;
+            this.lblJobTitle.Location = new System.Drawing.Point(34, 114);
+            this.lblJobTitle.Name = "lblJobTitle";
+            this.lblJobTitle.Size = new System.Drawing.Size(47, 13);
+            this.lblJobTitle.TabIndex = 16;
+            this.lblJobTitle.Text = "Job Title";
+            // 
+            // txtJobTitle
+            // 
+            this.txtJobTitle.Location = new System.Drawing.Point(91, 111);
+            this.txtJobTitle.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtJobTitle.Name = "txtJobTitle";
+            this.txtJobTitle.Size = new System.Drawing.Size(180, 20);
+            this.txtJobTitle.TabIndex = 2;
+            // 
+            // lblExtraTxt
+            // 
+            this.lblExtraTxt.AutoSize = true;
+            this.lblExtraTxt.Location = new System.Drawing.Point(34, 329);
+            this.lblExtraTxt.Name = "lblExtraTxt";
+            this.lblExtraTxt.Size = new System.Drawing.Size(55, 13);
+            this.lblExtraTxt.TabIndex = 18;
+            this.lblExtraTxt.Text = "Extra Text";
+            // 
+            // txtExtra
+            // 
+            this.txtExtra.Location = new System.Drawing.Point(91, 326);
+            this.txtExtra.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.txtExtra.Name = "txtExtra";
+            this.txtExtra.Size = new System.Drawing.Size(180, 20);
+            this.txtExtra.TabIndex = 17;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnAction;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 413);
+            this.Controls.Add(this.lblExtraTxt);
+            this.Controls.Add(this.txtExtra);
+            this.Controls.Add(this.lblJobTitle);
+            this.Controls.Add(this.txtJobTitle);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblFax);
+            this.Controls.Add(this.lblMob);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtFax);
+            this.Controls.Add(this.txtMob);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.btnAction);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Signature Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtMob;
+        private System.Windows.Forms.TextBox txtFax;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblMob;
+        private System.Windows.Forms.Label lblFax;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblJobTitle;
+        private System.Windows.Forms.TextBox txtJobTitle;
+        private System.Windows.Forms.Label lblExtraTxt;
+        private System.Windows.Forms.TextBox txtExtra;
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using SignatureEditor.Properties;
+using System.Globalization;
+
+namespace SignatureEditor
+{
+    public partial class MainForm : Form
+    {
+        List<string> _sig = new List<string>();
+        bool errorMsgTriggered = false;
+        string saveFileName, _year, _serverSigPath, _newSignaturePath, _localSigPath,_newSigImages;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            ReadSignatureTemplate();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _year = Settings.Default.YearOfSignature;
+            _localSigPath = Settings.Default.ServerSigLocation;
+            _serverSigPath = Settings.Default.ServerSigLocation;
+            _newSignaturePath = Settings.Default.NewSigLocation;
+            _newSigImages = Settings.Default.NewSigImages;
+            
+        }
+        private void WriteNewSignature(List<string> NewSig)
+        {
+            try
+            {
+                saveFileName = txtLname.Text.Remove(1); 
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < _sig.Count; i++)
+                {
+                    sb.AppendLine(this._sig[i].ToString());
+                }
+                
+                using (StreamWriter outfile = new StreamWriter(_serverSigPath + _year + "_" + this.txtFname.Text + saveFileName + ".htm"))
+                {
+                    outfile.Write(sb.ToString());                    
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); 
+            }
+            finally 
+            {
+                if (File.Exists(_serverSigPath + _year + "_" + this.txtFname.Text + saveFileName + ".htm"))
+                {
+                    if (copySignatureToLocalProfile())
+                    {
+                        MessageBox.Show("The Signature has been created successfully!\r\nThis program will now close.", "Signature Created", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        MessageBox.Show("There was an error copying the signature to the local computer!\r\nContact IT Support for help.\r\n\nThis program will now close.", "Signature Not Created", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        Environment.Exit(1);
+                    }
+                }
+            }
+        }
+        private bool copySignatureToLocalProfile()
+        {
+            string usr = Environment.UserName;
+            string usrProfile = @"C:\Documents and Settings\"+usr+ @"\Application Data\Microsoft\Signatures\";
+
+            try
+            {
+                if (File.Exists(_serverSigPath + _year + "_" + this.txtFname.Text + saveFileName + ".htm"))
+                {
+                    File.Copy(_serverSigPath + _year + "_" + this.txtFname.Text + saveFileName + ".htm", usrProfile +
+                        _year + "_" + this.txtFname.Text + saveFileName + ".htm", true);
+
+
+                    string[] files = Directory.GetFiles(_newSigImages);
+                    foreach (string s in files)
+                    {
+                        string fileName = Path.GetFileName(s);
+                        string destFile = Path.Combine(usrProfile, fileName);
+                        File.Copy(s, destFile, true);
+
+
+                    }
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+        private void validationMessage(string _msg)
+        {
+            //set bool to true
+            errorMsgTriggered = true;
+            //clear list
+            _sig.Clear();
+            MessageBox.Show(_msg, "Details Required!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);            
+        }
+        private void ReadSignatureTemplate()
+        {
+            try
+            {
+                string filePath = _newSignaturePath + "new_signature.htm";
+                string line;
+
+                if (File.Exists(filePath))
+                {
+                    StreamReader file = null;
+                    try
+                    {
+                        file = new StreamReader(filePath);
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            if (line.Contains("FirstnameLastname"))
+                            {
+                                if (this.txtFname.Text != "" && this.txtLname.Text != "")
+                                {
+                                    var names = line.Replace("FirstnameLastname", this.txtFname.Text + " " + this.txtLname.Text);
+                                    _sig.Add(names);
+                                }
+                                else { validationMessage("A First and Last name is required."); break; }
+
+                            }
+                            else if (line.Contains("JobTitle"))
+                            {
+                                if (this.txtJobTitle.Text != "")
+                                {
+                                    var jobTitle = line.Replace("JobTitle", this.txtJobTitle.Text);
+                                    _sig.Add(jobTitle);
+                                }
+                                else { validationMessage("A Job Title is required."); break; }
+
+                            }
+                            else if (line.Contains("Phone:"))
+                            {
+                                if (this.txtPhone.Text != "")
+                                {
+                                    if (validatePhoneNumbers(this.txtPhone.Text))
+                                    {
+                                        var phne = line.Replace("(07) 3371 7229", "(07) " + this.txtPhone.Text);
+                                        _sig.Add(phne);
+                                    }
+                                    else { validationMessage("A valid Phone number is required.\r\n"+ 
+                                        "In the following format: 1234 5678"); break; }
+                                }
+                                else { validationMessage("A Phone number is required."); break; }
+
+                            }
+                            else if (line.Contains("Mob:"))
+                            {
+                                if (this.txtMob.Text != "")
+                                {
+                                    if (validatePhoneNumbers(this.txtMob.Text))
+                                    {
+                                        var remComments1 = line.Remove(0, 5);
+                                        var remComments2 = remComments1.Remove(remComments1.Length - 3, 3);
+                                        var mobile = remComments2.Replace("0412 345 678", this.txtMob.Text);
+                                        _sig.Add(mobile);
+                                    }
+                                    else { validationMessage("A valid Mobile number is required.\r\n"+ 
+                                        "In the following format: 1234 5678"); break; }
+                                }
+                            }
+                            else if (line.Contains("Fax:"))
+                            {
+                                if (this.txtFax.Text != "")
+                                {
+                                    if (validatePhoneNumbers(this.txtFax.Text))
+                                    {
+                                        var fax = line.Replace("(07) 3371 7229", "(07) " + this.txtFax.Text);
+                                        _sig.Add(fax);
+                                    }
+                                    else { validationMessage("A valid Fax number is required."); break; }
+                                }
+                                else { _sig.Add(line); }
+                            }
+                            else if (line.Contains("Email:"))
+                            {
+                                if (this.txtEmail.Text != "")
+                                {
+                                    if (validateEmailAddress(this.txtEmail.Text))
+                                    {
+                                        var email = line.Replace("info@uqs.com.au", this.txtEmail.Text);
+                                        _sig.Add(email);
+                                    }
+                                    else { validationMessage("A valid Email address is required"); break; }
+                                }
+                                else { validationMessage("An Email address is required"); break; }
+                            }
+                            else
+                            {
+                                _sig.Add(line);
+                            }
+                        }
+                        if (errorMsgTriggered != true) { WriteNewSignature(_sig); }
+                    }
+                    finally
+                    {
+                        errorMsgTriggered = false;
+                        if (file != null)
+                            file.Close();
+                    }
+                }
+                else { MessageBox.Show("Email template \"new_signature.htm\" not found","Template not found!",MessageBoxButtons.OK,MessageBoxIcon.Error); }
+               
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private bool validateEmailAddress(string emailAddress)
+        {
+           
+          string patternStrict = @"^(([^<>()[\]\\.,;:\s@\""]+"
+
+                + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+
+                + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+                + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+
+                + @"[a-zA-Z]{2,}))$";
+
+          Regex reStrict = new Regex(patternStrict);
+
+                bool isStrictMatch = reStrict.IsMatch(emailAddress);
+
+                return isStrictMatch;
+            
+        }
+        private bool validatePhoneNumbers(string phoneNumber)
+        {
+            string patternStrict = @"^\d{4} [0-9]{4}$";//@"^\d\s+$"; @"^\d{1}-[0-9a-zA-Z]{6}$"
+
+            Regex reStrict = new Regex(patternStrict);
+
+            bool isStrictMatch = reStrict.IsMatch(phoneNumber);
+               
+            return isStrictMatch;
+        }
+    }
+}
